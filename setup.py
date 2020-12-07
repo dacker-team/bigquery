2e6c97fb7968a7b9f2dc56e03cb118b4a6ee6e6d
Pierre-François Lucet <pierre-francois@dacker.co>
2020-12-07 10:10:45 +0100

    packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='bigquery',
-    version='0.0.1',
+    version='0.0.3',
     description='Easily send data to Big Query',
     long_description=readme,
     author='Dacker',
@@ -18,7 +18,6 @@ setup(
         "dbstream>=0.0.19",
         "google-cloud-bigquery>=2.4.0",
         "googleauthentication>=0.0.11",
-        "pandas>=1.1.4",
         "google-cloud-bigquery-storage>=2.1.0"
     ],
     classifiers=[
