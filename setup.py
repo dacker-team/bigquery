6ca3f9c7b065e9c8a48d2eade737e6509bf7d962
Frédéric Le Roux <77618867+Freddacker@users.noreply.github.com>
2022-08-22 15:14:03 +0200

    Correcting requirements

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     python_requires='>=3',
     install_requires=[
-        "dbstream=0.1.15",
+        "dbstream>=0.1.15",
         "google-cloud-bigquery>=2.4.0",
         "googleauthentication>=0.0.12",
         "google-cloud-bigquery-storage>=2.1.0",
